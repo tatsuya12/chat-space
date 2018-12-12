fac17bf17c21aa76fec1676d0f639406751da995
tatsuya12 <tatsuya.0530.167@gmial.com>
2018-12-12 11:45:39 +0900

    imageを変数にしhtml共通化
    $('.form__message').val('')
    を
    $('.new_message')[0].reset();
    に修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,53 +1,22 @@
 $(function(){
   function buildHTML(message){
-    if (message.content && message.image){
-      var html =  `<div class='message'>
-                    <div class='heigher-message'>
-                      <div class='heigher-message__name'>
-                        ${message.user_name}
-                      </div>
-                      <div class='heigher-message__date'>
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class='lower-message'>
-                      <p class='lower-message__content'>
-                        ${message.content }
-                      </p>
-                      <img class="lower-message__image" src="${message.image}" />
-                    </div>
-                  </div>`
-    } else if (message.content) {
-      var html = `<div class='message'>
-                    <div class='heigher-message'>
-                      <div class='heigher-message__name'>
-                        ${message.user_name}
-                      </div>
-                      <div class='heigher-message__date'>
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class='lower-message'>
-                      <p class='lower-message__content'>
-                        ${message.content }
-                      </p>
-                    </div>
-                  </div>`
-    } else if (message.image) {
-      var html =  `<div class='message'>
-                    <div class='heigher-message'>
-                      <div class='heigher-message__name'>
-                        ${message.user_name}
-                      </div>
-                      <div class='heigher-message__date'>
-                        ${message.date}
-                      </div>
-                    </div>
-                    <div class='lower-message'>
-                      <img class="lower-message__image" src="${message.image}" />
-                    </div>
-                  </div>`
-    }
+    var image = message.image ?  `<img class="lower-message__image" src="${message.image}" />` : '' ;
+    var html =  `<div class='message'>
+      <div class='heigher-message'>
+        <div class='heigher-message__name'>
+          ${message.user_name}
+        </div>
+        <div class='heigher-message__date'>
+          ${message.date}
+        </div>
+      </div>
+      <div class='lower-message'>
+        <p class='lower-message__content'>
+          ${message.content }
+        </p>
+        ${image}
+      </div>
+    </div>`
     return html;
   }
   $('#new_message').on('submit', function(e){
@@ -64,8 +33,8 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html)
-      $('.form__message').val('')
+      $('.messages').append(html);
+      $('.new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
       $('.messages').animate({scrollTop: 200000});
     })
@@ -74,3 +43,5 @@ $(function(){
     })
   })
 });
+
+
