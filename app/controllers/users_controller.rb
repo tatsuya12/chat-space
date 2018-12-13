cdcb75defd03fca82fd32e09c6e0b0217d62a810
tatsuya12 <tatsuya.0530.167@gmial.com>
2018-12-13 10:38:00 +0900

    edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(29)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
